f3a7d29c255b9e45b0bb7a6c024e70f86ad7e1cb
VisattIpojuca <visattipojuca@gmail.com>
2025-06-17 14:27:39 -0300

    Add files via upload

--- faltas teste.py
+++ faltas teste.py
@@ -73,8 +73,9 @@ with col1:
     fig1 = px.histogram(
         df_filtrado,
         x="Tipo de Ausência",
-        color="Nome do Profissional",  # Legenda
+        color="Nome do Profissional",  # Na visualização, representa as Unidades
         title="Faltas por Tipo"
+        labels={"Nome do Profissional": "Unidades"}  # <-- legenda alterada aqui
     )
     st.plotly_chart(fig1, use_container_width=True)
 
